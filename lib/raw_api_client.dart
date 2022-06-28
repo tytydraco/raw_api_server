681a6fac22c37b5b97de066c1d4543717b21f968
Tyler Nijmeh <tylernij@gmail.com>
2022-06-28 13:41:29 -0700

    Give timeout in connect, not client class
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- lib/raw_api_client.dart
+++ lib/raw_api_client.dart
@@ -6,7 +6,6 @@ import 'package:raw_api_server/model/api_request.dart';
 class RawApiClient {
   final int port;
   final String host;
-  final Duration? timeout;
   final void Function(Socket)? onConnect;
   final void Function(Socket, Uint8List)? onReceive;
   final void Function(Socket, dynamic)? onError;
@@ -17,14 +16,15 @@ class RawApiClient {
   RawApiClient({
     required this.port,
     required this.host,
-    this.timeout,
     this.onConnect,
     this.onReceive,
     this.onError,
     this.onDone,
   });
 
-  Future<void> connect() async {
+  Future<void> connect({
+    Duration? timeout
+  }) async {
     socket = await Socket.connect(
       host,
       port,
